d16568d12cd1d63ddd8bef7d88920f34656def9a
Lalit Yadav <lalityadav.x17@gmail.com>
2025-07-24 19:17:37 -0500

    insertion sort

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
-nasm -f elf64 bubble.asm -o bubble.o
-ld bubble.o -o bubble
-./bubble
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <filename without extension>"
+    exit 1
+fi
+
+name="$1"
+nasm -f elf64 "$name.asm" -o "$name.o"
+ld "$name.o" -o "$name"
+./"$name"
